67e57e5d35a50f4d7735a5203e8ef5a1912cb524
mijung_82 <82928447+mimimi82@users.noreply.github.com>
2022-04-23 16:48:53 +0900

    Add files via upload

---
+++ elif 조건문 연습.py
@@ -0,0 +1,11 @@
+num1 = int(input("첫 번째 정수를 입력하시오: "))
+num2 = int(input("두 번째 정수를 입력하시오: "))
+
+if num1 > num2 :
+    print("첫 번째 정수가 더 큽니다.")
+
+elif num1 < num2 :
+    print("두 번째 정수가 더 큽니다.")
+
+else :
+    print("두 수가 똑같습니다.")
